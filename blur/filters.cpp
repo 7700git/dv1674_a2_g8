a7c9d87b5c603671a395d4d59ca505f0102dcb13
vaishnavi <vaishnavibojja0852@gmail.com>
2025-10-19 15:48:06 +0200

    caching image dimension outside loop and resolved bugs in previous commit

--- blur/filters.cpp
+++ blur/filters.cpp
@@ -6,6 +6,7 @@ Author: David Holmqvist <daae19@student.bth.se>
 #include "matrix.hpp"
 #include "ppm.hpp"
 #include <cmath>
+#include <cstdef>
 
 namespace Filter
 {
@@ -29,11 +30,13 @@ namespace Filter
 	//move the weight calc to outside of nested loops to exponentially decrease calls to this function
         double w[Gauss::max_radius]{};
         Gauss::get_weights(radius, w);
-	//the loops are based on scanning columns first which uses more cache, hence flipping the loop inside out is more efficient
 
-        for (auto y{0}; y < dst.get_y_size(); y++)
+	std::size_t width = dst.get_x_size();
+	std::size_t height = dst.get_y_size();
+	//the loops are based on scanning columns first which uses more cache, hence flipping the loop inside out is more efficient
+        for (std::size_t y=0; y < height; ++y)
         {
-            for (auto x{0}; x < dst.get_x_size(); x++)
+            for (std::size_t x=0; x < width; ++x)
             {
 
                 // unsigned char Matrix::r(unsigned x, unsigned y) const
@@ -43,10 +46,10 @@ namespace Filter
 
                 auto r{w[0] * dst.r(x, y)}, g{w[0] * dst.g(x, y)}, b{w[0] * dst.b(x, y)}, n{w[0]};
 
-                for (auto wi{1}; wi <= radius; wi++)
+                for (int wi=1; wi <= radius; ++wi)
                 {
-                    auto wc{w[wi]};
-                    auto x2{x - wi};
+                    double wc = w[wi];
+                    int x2 = static_cast<int>(x) - wi;
                     if (x2 >= 0)
                     {
                         r += wc * dst.r(x2, y);
@@ -54,8 +57,8 @@ namespace Filter
                         b += wc * dst.b(x2, y);
                         n += wc;
                     }
-                    x2 = x + wi;
-                    if (x2 < dst.get_x_size())
+                    x2 = static_cast<int>x + wi;
+                    if (x2 < static_cast<int>(width))
                     {
                         r += wc * dst.r(x2, y);
                         g += wc * dst.g(x2, y);
@@ -69,20 +72,20 @@ namespace Filter
             }
         }
 	//same alteration in these loops as above
-        double w[Gauss::max_radius]{};
-        Gauss::get_weights(radius, w);
+        //double w[Gauss::max_radius]{};
+        //Gauss::get_weights(radius, w);
 	//same change as above, flipping the loop matrix inside out
-        for (auto y{0}; y < dst.get_y_size(); y++)
+        for (std::size_t y = 0; y < height; ++y)
         {
-            for (auto x{0}; x < dst.get_x_size(); x++)
+            for (std::size_t x = 0; x < width; ++x)
             {
 
                 auto r{w[0] * scratch.r(x, y)}, g{w[0] * scratch.g(x, y)}, b{w[0] * scratch.b(x, y)}, n{w[0]};
 
-                for (auto wi{1}; wi <= radius; wi++)
+                for (int wi = 1; wi <= radius; ++wi)
                 {
-                    auto wc{w[wi]};
-                    auto y2{y - wi};
+                    double wc = w[wi];
+                    int y2 = static_cast<int>(y) - wi;
                     if (y2 >= 0)
                     {
                         r += wc * scratch.r(x, y2);
@@ -90,8 +93,8 @@ namespace Filter
                         b += wc * scratch.b(x, y2);
                         n += wc;
                     }
-                    y2 = y + wi;
-                    if (y2 < dst.get_y_size())
+                    y2 = statis_cast<int>(y) + wi;
+                    if (y2 < static_cast<int>(height))
                     {
                         r += wc * scratch.r(x, y2);
                         g += wc * scratch.g(x, y2);
